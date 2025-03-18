d42fefa71c8d6d2224f94e7eca07d33eb4625e13
Andrey Mladenov <amladenov@technologica.com>
2025-03-18 17:03:44 +0200

    Dev Env

--- infrastructure/create_static_webapp.bat
+++ infrastructure/create_static_webapp.bat
@@ -1,7 +1,7 @@
 @echo off
 SET RESOURCE_GROUP=the-mladens-gift-finder
 SET LOCATION=westeurope
-SET APP_NAME=the-mladens-gift-finder
+SET APP_NAME=the-mladens-gift-finder-dev
 SET GITHUB_REPO=https://github.com/The-Mladens/gift-finder
 
 REM az login --tenant 13a3cd93-9724-47f8-a7d7-c467004b3519 --subscription ecb983df-e8c1-496e-bf7b-7fd270c62f1e
